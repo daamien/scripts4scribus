d0f03a10876c9e258305218c97085635c047df92
Damien Clochard <damien@taadeem.net>
2011-05-22 01:32:27 +0200

    Watermark : improved behaviour

--- watermark/watermark.py
+++ watermark/watermark.py
@@ -28,40 +28,53 @@ draft  = "DRAFT"
 #draft = "BROUILLON"
 
 layer_id = "scribus_automatic_watemark"
+text_id = "scribus_automatic_watemark_text"
 
 defineColor("gray", 11, 11, 11, 11)           	# Set your own color here
 
 if haveDoc():
 
-    page = 1
-    pagenum = pageCount()
+
     T = []
     content = []
 
     draft = valueDialog("Watermark", "Enter the draft message or leave it empty to delete the existing watermark" ,draft)
     L = len(draft)                      	# The length of the word 
                                               	# will determine the font size    
+  
+    al = getActiveLayer()                     	# Identify the working layer
+    u  = getUnit()                            	# Get the units of the document
 
-    while (page <= pagenum):
-	gotoPage(page)
-	u  = getUnit()                            	# Get the units of the document
-	al = getActiveLayer()                     	# Identify the working layer
-	setUnit(UNIT_MILLIMETERS)                 	# Set the document units to mm,                                            
-	(w,h) = getPageSize()                     	# needed to set the text box size
+    if (L != 0) :
+	createLayer(layer_id)
+	setActiveLayer(layer_id)
+        
+        page = 1
+	pagenum = pageCount()    
+	while (page <= pagenum):
+		gotoPage(page)
+		setUnit(UNIT_MILLIMETERS)                 	# Set the document units to mm,                                            
+		(w,h) = getPageSize()                     	# needed to set the text box size
 
-	if (L != 0) :
-		createLayer(layer_id)
-		setActiveLayer(layer_id)
-
-		T = createText(w/6, 6*h/10 , h, w/2)  # Create the text box
+		T = createText(w/6, 6*h/10 , h, w/2,text_id+str(page))    # Create the text box
 		setText(draft, T)                         	# Insert the text
 		setTextColor("gray", T)                  	# Set the color of the text
 		setFontSize((w/210)*(180 - 10*L), T)     	# Set the font size according to length and width
-
 		rotateObject(45, T)                      	# Turn it round antclockwise 45 degrees
-		setUnit(u)                               	# return to original document units
-		setActiveLayer(al)                       	# return to the original active layer
-	else :
-		deleteLayer(layer_id)
 
-	page += 1
+		page += 1
+    else :
+	setActiveLayer(layer_id)
+
+        page = 1
+	pagenum = pageCount()    
+	while (page <= pagenum):
+		gotoPage(page)
+		deleteObject(text_id+str(page))    # Delete the text box
+		page += 1
+
+	deleteLayer(layer_id)
+
+	
+    setUnit(u)                               	# return to original document units
+    setActiveLayer(al)                       	# return to the original active layer
